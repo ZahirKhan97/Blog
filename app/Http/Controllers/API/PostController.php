feb6674a9a517180fb04738bb6e0075aa645c189
ZahirKhan97 <GitHub: mzahirkhan302@gmail.com>
2024-09-21 03:19:47 +0500

    Image Manipulation Completed

--- app/Http/Controllers/API/PostController.php
+++ app/Http/Controllers/API/PostController.php
@@ -7,7 +7,10 @@ use App\Http\Controllers\API\BaseController as BaseController;
 use App\Models\Post;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Validator;
+use Intervention\Image\ImageManager;
+use Intervention\Image\Drivers\Gd\Driver;
 
 class PostController extends BaseController
 {
@@ -32,16 +35,29 @@ class PostController extends BaseController
         $validator = Validator::make($request->all(), [
             'title' => 'required|min:3',
             'content' => 'required|min:20',
-            'image' => 'mimes:jpg,jpeg,png,gif'
+            'image' => 'image|mimes:jpg,jpeg,png,gif'
         ]);
         if ($validator->fails()) {
             return $this->sendError('Validation Errors', $validator->errors(), 401);
         }
 
+        if ($request->hasFile('image')) {
+            $manager = new ImageManager(new Driver());
+            $file = $request->file('image');
+            $ext = $file->getClientOriginalExtension();
+            $filename = time() . '.' . $ext;
+            $path = public_path('uploads/blog/' . $filename);
+            $image = $manager->read($file->getRealPath());
+            $image->cover(300, 300);
+            $image->save($path);
+        } else {
+            $filename = '';
+        }
+
         $post = Post::create([
             'title' => $request->title,
             'content' => $request->content,
-            'image' => $request->image,
+            'image' => $filename,
             'status' => $request->status,
             'user_id' => Auth::user()->id,
         ]);
@@ -80,10 +96,26 @@ class PostController extends BaseController
                 return $this->sendError('Validation Errors', $validator->errors(), 401);
             }
 
+            if ($request->hasFile('image')) {
+                if ($post->image && File::exists(public_path('uploads/blog/' . $post->image))) {
+                    File::delete(public_path('uploads/blog/' . $post->image));
+                }
+                $manager = new ImageManager(new Driver());
+                $file = $request->file('image');
+                $ext = $file->getClientOriginalExtension();
+                $filename = time() . '.' . $ext;
+                $path = public_path('uploads/blog/' . $filename);
+                $image = $manager->read($file->getRealPath());
+                $image->cover(300, 300);
+                $image->save($path);
+            } else {
+                $filename = $post->image;
+            }
+
             $post->update([
                 'title' => $request->title,
                 'content' => $request->content,
-                'image' => $request->image,
+                'image' => $filename,
                 'status' => $request->status,
                 'user_id' => Auth::user()->id,
             ]);
@@ -104,6 +136,9 @@ class PostController extends BaseController
 
         if ($post->user_id !== Auth::id()) {
             return $this->sendError('You cannot delete this Post', 'You are not Authorized', 403);
+        }
+        if ($post->image && File::exists(public_path('uploads/blog/' . $post->image))) {
+            File::delete(public_path('uploads/blog/' . $post->image));
         }
         $post->delete();
         return $this->sendResponse(null, 'Post Deleted Successfully');
